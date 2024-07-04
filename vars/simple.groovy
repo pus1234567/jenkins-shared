a86cca94abf3fad4f52618f83b192ea582cb9ca2
Panagiotis Semertzidis <panagiotis.semertzidis@eurodyn.com>
2024-07-04 11:56:46 +0300

    Update

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -6,13 +6,38 @@ def call(body) {
     body()
 
     pipeline {
-        agent any
-        stages {
-            stage("Hi") {
-                steps {
-                    sh "echo Hi"                   
-                }
+    environment {
+        NEXUS_D = credentials(${pipelineParams.n_pass})
+    }
+  agent {
+    kubernetes {
+      yaml """
+kind: Pod
+spec:
+  containers:
+  - name: dind
+    image: docker:dind
+    securityContext:
+      privileged: true
+    args:
+      - "--insecure-registry=192.168.2.5:8082"
+"""
+    }
+  }
+  stages {
+      stage('Build Maven'){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: ${pipelineParams.git_url}]]])
+                mavenBuild()
             }
     }
+    stage('Docker build') {
+      steps {
+        container(name: 'dind') {
+            dockerBuildPush(name:${pipelineParams.name}, tag:${pipelineParams.tag} address:${pipelineParams.address}, repo:${pipelineParams.repo}, username:${pipelineParams.username}, pass:NEXUS_D)
+        }
+      }
+    }
+  }
 }
 }

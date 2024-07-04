db52ebe4f5550b655c13fb3d84f8c335efc7ebd6
Panagiotis Semertzidis <panagiotis.semertzidis@eurodyn.com>
2024-07-04 11:19:34 +0300

    Update

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -1,8 +1,8 @@
 def call(body) {
     
     def pipelineParams= [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body()
 
     pipeline {

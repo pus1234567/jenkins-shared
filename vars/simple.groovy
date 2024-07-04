6696e63b53960296f62490dec016c55edd102a50
pus1234567 <115112829+pus1234567@users.noreply.github.com>
2024-07-04 13:10:33 +0300

    Update simple.groovy

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -27,9 +27,20 @@ spec:
       stage('Build Maven'){
             steps{
                 checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: pipelineParams.git_url]]])
-                echo "${pipelineParams.repo}"
-                echo "${pipelineParams.address}"
+                mavenBuild()
             }
+    }
+      stage('Docker build') {
+      steps {
+        container(name: 'dind') {
+            sh """
+                    docker build -f src/main/docker/Dockerfile.jvm -t ${pipelineParams.name} .
+                    docker login ${pipelineParams.address} -u=${pipelineParams.username} -p=${NEXUS_D}
+                    docker tag ${pipelineParams.name}:${pipelineParams.tag} ${pipelineParams.repo}/${pipelineParams.name}
+                    docker push ${pipelineParams.repo}/${pipelineParams.name}
+                """
+        }
+      }
     }
   }
 }

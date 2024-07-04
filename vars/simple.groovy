585754dc10c191209ac051725d848a6fcb52cba5
pus1234567 <115112829+pus1234567@users.noreply.github.com>
2024-07-04 12:44:31 +0300

    Update simple.groovy

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -33,7 +33,7 @@ spec:
       stage('Docker build') {
       steps {
         container(name: 'dind') {
-            dockerBuildPush(name:"ex1", tag:"latest" address:"192.168.2.5:8082", repo:"192.168.2.5:8082/repository/docker-images", username:"admin", pass:NEXUS_D)
+            dockerBuildPush(name:"ex1", tag:"latest", address:"192.168.2.5:8082", repo:"192.168.2.5:8082/repository/docker-images", username:"admin", pass:NEXUS_D)
         }
       }
     }

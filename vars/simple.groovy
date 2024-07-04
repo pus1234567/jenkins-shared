034301465430abbeb6b9bad454d0a1a165629b1c
pus1234567 <115112829+pus1234567@users.noreply.github.com>
2024-07-04 12:20:55 +0300

    Update simple.groovy

--- vars/simple.groovy
+++ vars/simple.groovy
@@ -24,7 +24,7 @@ spec:
   stages {
       stage('Build Maven'){
             steps{
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: pipelineParams.git_url]]])
+                echo "Hi"
             }
     }
   }

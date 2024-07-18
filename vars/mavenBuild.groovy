293ffc45dd473a65b15e84ba5b89110df22efe9a
pus1234567 <115112829+pus1234567@users.noreply.github.com>
2024-07-18 15:30:15 +0300

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,29 @@
-def call() {
-    sh  '''
-            chmod +x mvnw
-            ./mvnw package
-        '''
+def call(Map config = [:]) {
+    pipeline {
+        agent {
+    kubernetes {
+      yaml """
+kind: Pod
+spec:
+  containers:
+  - name: dind1
+    image: docker:dind
+    securityContext:
+      privileged: true
+"""
+    }
+  }
+        stages {
+            stage('Build and Push Agent Images') {
+                steps {
+                    container('dind') {
+                        for(p in config) {
+                            sh 'mkdir ${p.value}'
+                            dir(p.value)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
